d377c6c70d38c0720fda49755c5f46fc8505bfd0
Antonio Alves Santos Marques <antonio.marques@outlook.com>
2024-08-05 03:03:37 +0100

    Added PersonComparer class to improve the unit tests.

--- Credas.NameSorter.Tests/NameSortingServiceTests.cs
+++ Credas.NameSorter.Tests/NameSortingServiceTests.cs
@@ -12,6 +12,22 @@ namespace NameSorter.Tests.Services
             _service = new NameSortingService();
         }
 
+        // Custom comparer for Person objects to check equality based on FirstName, MiddleNames, and LastName
+        public class PersonComparer : IEqualityComparer<Person>
+        {
+            public bool Equals(Person x, Person y)
+            {
+                return x.FirstName == y.FirstName &&
+                       x.MiddleNames == y.MiddleNames &&
+                       x.LastName == y.LastName;
+            }
+
+            public int GetHashCode(Person obj)
+            {
+                return HashCode.Combine(obj.FirstName, obj.MiddleNames, obj.LastName);
+            }
+        }
+
         #region SortNames
         [Fact]
         public void SortNames_ShouldSortByLastNameThenGivenNames()
@@ -51,10 +67,7 @@ namespace NameSorter.Tests.Services
             var sortedNames = _service.SortNames(unsortedNames);
 
             // Assert
-            Assert.Equal(
-                expectedSortedNames.Select(p => p.ToString()),
-                sortedNames.Select(p => p.ToString())
-            );
+            Assert.Equal(expectedSortedNames, sortedNames, new PersonComparer());
         }
         #endregion
 
@@ -83,13 +96,52 @@ namespace NameSorter.Tests.Services
             var names = _service.ReadNamesFromFile(filePath);
 
             // Assert
-            Assert.Equal(
-                expectedNames.Select(p => p.ToString()),
-                names.Select(p => p.ToString())
-            );
+            Assert.Equal(expectedNames, names, new PersonComparer());
 
             // Cleanup
             File.Delete(filePath);
+        }
+
+        [Fact]
+        public void ReadNamesFromFile_ShouldHandleUnexpectedData()
+        {
+            // Arrange
+            var filePath = Path.GetTempFileName();
+            var fileContent = new[]
+            {
+                "Janet Parsons",
+                "",
+                "Vaughn Lewis",
+                "Adonis Julius Archer",
+                " "
+            };
+            File.WriteAllLines(filePath, fileContent);
+
+            var expectedNames = new List<Person>
+            {
+                new Person("Janet Parsons"),
+                new Person("Vaughn Lewis"),
+                new Person("Adonis Julius Archer")
+            };
+
+            // Act
+            var names = _service.ReadNamesFromFile(filePath);
+
+            // Assert
+            Assert.Equal(expectedNames, names, new PersonComparer());
+
+            // Cleanup
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void ReadNamesFromFile_ShouldThrowException_WhenFileDoesNotExist()
+        {
+            // Arrange
+            var filePath = "nonexistentfile.txt";
+
+            // Act & Assert
+            Assert.Throws<FileNotFoundException>(() => _service.ReadNamesFromFile(filePath));
         }
         #endregion
 
@@ -121,53 +173,6 @@ namespace NameSorter.Tests.Services
 
             // Cleanup
             File.Delete(filePath);
-        }
-        #endregion
-
-        #region ReadNamesFromFile
-        [Fact]
-        public void ReadNamesFromFile_ShouldHandleUnexpectedData()
-        {
-            // Arrange
-            var filePath = Path.GetTempFileName();
-            var fileContent = new[]
-            {
-                "Janet Parsons",
-                "",
-                "Vaughn Lewis",
-                "Adonis Julius Archer",
-                " "
-            };
-            File.WriteAllLines(filePath, fileContent);
-
-            var expectedNames = new List<Person>
-            {
-                new Person("Janet Parsons"),
-                new Person("Vaughn Lewis"),
-                new Person("Adonis Julius Archer")
-            };
-
-            // Act
-            var names = _service.ReadNamesFromFile(filePath);
-
-            // Assert
-            Assert.Equal(
-                expectedNames.Select(p => p.ToString()),
-                names.Select(p => p.ToString())
-            );
-
-            // Cleanup
-            File.Delete(filePath);
-        }
-
-        [Fact]
-        public void ReadNamesFromFile_ShouldThrowException_WhenFileDoesNotExist()
-        {
-            // Arrange
-            var filePath = "nonexistentfile.txt";
-
-            // Act & Assert
-            Assert.Throws<FileNotFoundException>(() => _service.ReadNamesFromFile(filePath));
         }
         #endregion
     }
